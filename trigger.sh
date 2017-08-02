0c617e0253a5bbf1d9c54bdd15c4e975e3f869f7
Juan Pablo Genovese <juanpgenovese@gmail.com>
2017-08-02 04:24:52 +0000

    Add customizable fly target

--- trigger.sh
+++ trigger.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
-set -e -x
+set -e 
 
-fly sp -t test configure -c pipeline.yml -p redis --load-vars-from creds.yml --load-vars-from params.yml -n
-fly -t test up -p redis
-fly -t test trigger-job -j redis/prepare-deployment --watch
-fly -t test trigger-job -j redis/deploy-redis --watch
+fly_target=$1
+
+fly sp -t $fly_target configure -c pipeline.yml -p redis --load-vars-from creds.yml --load-vars-from params.yml -n
+fly -t $fly_target up -p redis
+fly -t $fly_target trigger-job -j redis/prepare-deployment --watch
+fly -t $fly_target trigger-job -j redis/deploy-redis --watch
