406d5ab54ecfffedf92131bfbe32c444491040a7
Juan Pablo Genovese <juanpgenovese@gmail.com>
2017-08-02 03:03:27 +0000

    Add separate job

--- trigger.sh
+++ trigger.sh
@@ -4,5 +4,5 @@ set -e -x
 
 fly sp -t test configure -c pipeline.yml -p redis --load-vars-from creds.yml --load-vars-from params.yml -n
 fly -t test up -p redis
-fly -t test trigger-job -j redis/deploy-redis
-fly -t test watch -j redis/deploy-redis
+fly -t test trigger-job -j redis/prepare-deployment --watch
+fly -t test trigger-job -j redis/deploy-redis --watch
