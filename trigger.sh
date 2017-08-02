23834575d7eab7f0f152d35821a26e7029f22e0d
Juan Pablo Genovese <juanpgenovese@gmail.com>
2017-08-02 02:38:09 +0000

    Change job name

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
+set -e -x
+
 fly sp -t test configure -c pipeline.yml -p redis --load-vars-from creds.yml --load-vars-from params.yml -n
 fly -t test up -p redis
-fly -t test trigger-job -j redis/job-deploy
-fly -t test watch -j redis/job-deploy
+fly -t test trigger-job -j redis/deploy-redis
+fly -t test watch -j redis/deploy-redis
